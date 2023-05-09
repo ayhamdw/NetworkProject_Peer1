f6ccdbc0b3a328eb21509f5e3e31d7a0a1b4914f
ayhamdw <s12029113@stu.najah.edu>
2023-05-09 20:59:49 +0300

    Everything expect sendAll

--- src/main/java/com/example/hw2client/HelloApplication.java
+++ src/main/java/com/example/hw2client/HelloApplication.java
@@ -1,9 +1,6 @@
 package com.example.hw2client;
-
-import com.jfoenix.controls.JFXListCell;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -19,23 +17,21 @@ import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collections;
-
+import java.util.Random;
 public class HelloApplication extends Application{
-
-
-    @Override
-    public void start(Stage stage) throws IOException {
-
-        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Parent root = loader.load();
-        HelloController load = loader.getController();
+    public void startServerLogic(HelloController controller) {
         byte[] receiveData = new byte[1024];
-        ObservableList<Label> items = load.MessageView.getItems();
-        DatagramSocket serverSocket = new DatagramSocket(9876);
+        ObservableList<Label> items = controller.MessageView.getItems();
+        ObservableList<String> items1 = controller.onlineUsers.getItems();
         new Thread(() -> {
             try {
+                Random random = new Random();
+                int port = random.nextInt(65535)+1;
+                System.out.println(port);
+                FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+                Parent root = loader.load();
+                HelloController load = loader.getController();
+                DatagramSocket serverSocket = new DatagramSocket((port));
                 while (true) {
                     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
                     LocalDateTime now = LocalDateTime.now();
@@ -43,32 +39,71 @@ public class HelloApplication extends Application{
                     serverSocket.receive(receivePacket);
                     InetAddress IPAddress = receivePacket.getAddress(); // the address of client
                     String result = new String(receivePacket.getData(), 0, receivePacket.getLength(), StandardCharsets.UTF_8);
-                    if (result.equals("Clear")) {
-                        Platform.runLater(() -> items.clear());
-                    } else {
-                        try {
-                            int index = Integer.parseInt(result);
-                            if (index >= 0) {
-                                Platform.runLater(() -> items.remove(index));
+                    if (result.contains("Logout,Now")){
+                        JOptionPane.showMessageDialog(null, "Inside");
+                        String [] strings = result.split("\n");
+                        JOptionPane.showMessageDialog(null , strings[1]);
+                        for (int i = 0; i < items1.size(); i++){
+                            if(items1.get(i).contains(strings[1])) {
+                                int finalI = i;
+                                Platform.runLater(()->items1.remove(finalI));
                             }
-                        } catch (Exception ex) {
-                            Label label = new Label(result + " (" + now.format(dtf) + ")");
-                            label.setTextFill(Color.GREEN);
-                            label.setStyle("-fx-font-family: Cambria;");
-                            Platform.runLater(() -> items.add(label));
                         }
                     }
+                    else if (result.contains(":")) {
+                        Refactor(items, dtf, now, result);
+                    } else if (result.contains(",") && result.length() > 10) {
+                        int flag = 1;
+                        if (items1.size() == 0) {
+                            Platform.runLater(()-> items1.add(result));
+                        }
+                        else {
+                            for (int i = 0; i < items1.size(); i++) {
+                                if (controller.onlineUsers.getItems().get(i).equals(result)) {
+                                    flag = 0;
+                                    break;
+                                }
+                            }
+                            if (flag == 1)
+                                Platform.runLater(() -> items1.add(result));
+                        }
+                    } else if (result.equals("Clear")) {
+                        Platform.runLater(() -> items.clear());
+                    } else {
+                        Refactor(items, dtf, now, result);
+                    }
                 }
-
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }).start();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        HelloController load = loader.getController();
+        startServerLogic(load);
+
         Scene scene = new Scene(root, 918, 566);
-        stage.setTitle("Server");
+        stage.setTitle("Client");
         stage.setScene(scene);
-        //stage.setResizable(false);
         stage.show();
+    }
+
+    private void Refactor(ObservableList<Label> items, DateTimeFormatter dtf, LocalDateTime now, String result) {
+        try {
+            int index = Integer.parseInt(result);
+            if (index >= 0) {
+                Platform.runLater(() -> items.remove(index));
+            }
+        } catch (Exception ex) {
+            Label label = new Label(result + " (" + now.format(dtf) + ")");
+            label.setTextFill(Color.GREEN);
+            label.setStyle("-fx-font-family: Cambria;");
+            Platform.runLater(() -> items.add(label));
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/hw2client/HelloController.java
+++ src/main/java/com/example/hw2client/HelloController.java
@@ -1,27 +1,43 @@
 package com.example.hw2client;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
 
 public class HelloController implements Initializable {
-     InetAddress address;
+
+     InetAddress address = InetAddress.getLocalHost();
     @FXML
     private TextField LocalIP;
 
     @FXML
-    private TextField LocalPort;
+    public TextField LocalPort;
 
     @FXML
     private JFXTextArea MessageArea;
@@ -35,17 +51,39 @@ public class HelloController implements Initializable {
     @FXML
     private TextField RemotePort;
 
-    @FXML
-    private TextField StatusView;
 
+
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordTextField;
+    @FXML
+    private TextField TCPIP;
+    @FXML
+    private TextField TCPPort;
+    @FXML
+    public JFXListView <String> onlineUsers;
+    @FXML
+    private JFXComboBox <String> availableInterface;
+    @FXML
+    Label label ;
+    String name;
 
     public HelloController() throws UnknownHostException {
     }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        MessageArea.setText("");
+        LocalIP.setText(address.getHostAddress());
+        availableInterface.getItems().add("WIFI: " + address.getHostAddress());
+        availableInterface.setValue(availableInterface.getItems().get(0));
+//        LocalPort.setText(String.valueOf(port));
+    }
 
     @FXML
-    void ClearAllButton(ActionEvent event) throws IOException {
+    void ClearAllButton() throws IOException {
         DatagramSocket clientSocket = new DatagramSocket();
-        byte[] sendData = new byte[1024];
+        byte[] sendData;
         sendData = ("Clear").getBytes();
         InetAddress address = InetAddress.getByName(RemoteIP.getText()); // get the destination ip
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, Integer.parseInt(RemotePort.getText()));
@@ -53,12 +91,12 @@ public class HelloController implements Initializable {
         MessageView.getItems().clear();
     }
     @FXML
-    void ClearButton(ActionEvent event) throws IOException {
+    void ClearButton() throws IOException {
         int index = MessageView.getSelectionModel().getSelectedIndex();
         if (index >= 0) {
             MessageView.getItems().remove(index);
             DatagramSocket clientSocket = new DatagramSocket();
-            byte[] sendData = new byte[1024];
+            byte[] sendData;
             sendData = String.valueOf(index).getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, Integer.parseInt(RemotePort.getText()));
             clientSocket.send(sendPacket);
@@ -68,18 +106,23 @@ public class HelloController implements Initializable {
     }
 
     @FXML
-    void SendButton(ActionEvent event) throws IOException {
+    void SendButton() throws IOException {
         ObservableList<Label> items = MessageView.getItems();
         if (RemoteIP.getText().isEmpty() || RemotePort.getText().isEmpty())
             JOptionPane.showMessageDialog(null, "Please Enter The Port Number or The IP", "Error Message", JOptionPane.ERROR_MESSAGE);
         else {
+
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
             LocalDateTime now = LocalDateTime.now();
             Label label = new Label();
             String text = MessageArea.getText();
             DatagramSocket clientSocket = new DatagramSocket();
-            byte[] sendData = new byte[1024];
-            sendData = (address.getHostName() + ": " + text).getBytes();
+            byte[] sendData;
+
+            if(check())
+                name = usernameTextField.getText();
+            else name = address.getHostName();
+            sendData = (name+ ": " + text).getBytes();
             InetAddress address = InetAddress.getByName(RemoteIP.getText()); // get the destination ip
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, Integer.parseInt(RemotePort.getText()));
             clientSocket.send(sendPacket);
@@ -92,24 +135,109 @@ public class HelloController implements Initializable {
         }
     }
 
+    public boolean check() throws FileNotFoundException {
+        boolean flag = false;
+        Scanner scanner = new Scanner(new File("D:\\SoftwareProject\\HW2Client\\src\\main\\java\\com\\example\\hw2client\\username.txt"));
+        while (scanner.hasNextLine()) {
+            String string = scanner.nextLine();
+            String[] strings = string.split(" ");
+            if (strings[0].equals(usernameTextField.getText()) && strings[1].equals(passwordTextField.getText())) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+
     @FXML
-    void TestButton(ActionEvent event) {
+    void Login() {
+        if (usernameTextField.getText().isEmpty() || passwordTextField.getText().isEmpty() )
+            JOptionPane.showMessageDialog(null, "Please Enter the Password and Username");
+        else if (TCPIP.getText().isEmpty() || TCPPort.getText().isEmpty())
+            JOptionPane.showMessageDialog(null , "Please Enter TCP IP and PortNumber");
+        else if (LocalPort.getText().isEmpty() || LocalIP.getText().isEmpty())
+            JOptionPane.showMessageDialog(null , "Please Enter IP and PortNumber");
+        else {
+            try {
+                String username = usernameTextField.getText();
+                String password = passwordTextField.getText();
+                String portnumber = LocalPort.getText();
+                String data = username + "\n" + password + "\n" + portnumber + "\n";
 
-    }
-    public void addSentence(String sentence) {
-       // MessageView.getItems().add(sentence);
-    }
+                Socket socket = new Socket(TCPIP.getText(),Integer.parseInt(TCPPort.getText()));
+                OutputStream outputStream = socket.getOutputStream();
+                outputStream.write(data.getBytes());
+                outputStream.flush();
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        try {
-            MessageArea.setText("");
-            address = InetAddress.getLocalHost(); // get my device name and its ip
-            LocalIP.setText(address.getHostAddress()); // get the IP from address
-            LocalPort.setText("9876");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+                // Close the socket
+                socket.close();
+            }
+            catch(IOException ex){
+                System.out.println(ex);
+                }
+            }
+        }
+    @FXML
+    void Logout(ActionEvent event) {
+        if (usernameTextField.getText().isEmpty() || passwordTextField.getText().isEmpty())
+            JOptionPane.showMessageDialog(null, "Please Enter the Password and Username");
+        else if (TCPIP.getText().isEmpty() || TCPPort.getText().isEmpty())
+            JOptionPane.showMessageDialog(null , "Please Enter TCP IP and PortNumber");
+        else {
+            try {
+                String username = usernameTextField.getText();
+                String data = username + "\n" + "Logout" + "\n";
+                Socket socket = new Socket(TCPIP.getText(),Integer.parseInt(TCPPort.getText()));
+                OutputStream outputStream = socket.getOutputStream();
+                outputStream.write(data.getBytes());
+                outputStream.flush();
+
+                // Close the socket
+                socket.close();
+                if (!onlineUsers.getItems().isEmpty()){
+                    for (int i = 0 ; i < onlineUsers.getItems().size(); i++) {
+                        DatagramSocket clientSocket = new DatagramSocket();
+                        byte[] sendData = new byte[1024];
+                        String [] strings = onlineUsers.getItems().get(i).split(",");
+                        sendData = ("Logout,Now" + "\n" + usernameTextField.getText()).getBytes();
+                        InetAddress address = InetAddress.getByName(strings[1]); // get the destination ip
+                        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, Integer.parseInt(strings[2]));
+                        clientSocket.send(sendPacket);
+                    }
+                    usernameTextField.setText("");
+                    passwordTextField.setText("");
+                    name= address.getHostName();
+                    onlineUsers.getItems().clear();
+                }
+            }
+            catch(IOException e){
+                //JOptionPane.showMessageDialog(null , "Client: Please Enter Correct IP and Port Number");
+                System.out.println(e);
+            }
         }
 
     }
+
+    @FXML
+    public void getText(Event event) {
+//        String [] selectedItem = onlineUsers.getSelectionModel().getSelectedItem().split(",");
+//        if (selectedItem != null) {
+//            RemoteIP.setText(selectedItem[1]);
+//            RemotePort.setText(selectedItem[2]);
+//        }
+    }
+    @FXML
+    void addInterface(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+        HelloController newController = loader.getController();
+        HelloApplication application = new HelloApplication();
+        application.startServerLogic(newController);
+        Stage newStage = new Stage();
+        Scene scene = new Scene(root, 918, 566);
+        newStage.setScene(scene);
+        newStage.show();
+    }
+
+
 }
